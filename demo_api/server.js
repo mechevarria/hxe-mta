50c7f0cc1a1755f0a62286100097e0642c4f9ac3
Mike Echevarria <michael.echevarria@sapns2.com>
2019-10-25 11:03:40 -0400

    add logging for cloudfoundry environment

--- demo_api/server.js
+++ demo_api/server.js
@@ -4,6 +4,8 @@
 var xsjs  = require("@sap/xsjs");
 var xsenv = require("@sap/xsenv");
 var port  = process.env.PORT || 3000;
+var vcap = process.env.VCAP_SERVICES;
+console.log(vcap);
 
 var options = {
 	anonymous : true, // remove to authenticate calls
