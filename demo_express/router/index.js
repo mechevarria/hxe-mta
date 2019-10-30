6ddb0ab5ad8867300a07c420303e5e0509599893
Mike Echevarria <michael.echevarria@sapns2.com>
2019-10-30 12:59:53 -0400

    add default index handler and other routes

--- demo_express/router/index.js
+++ demo_express/router/index.js
@@ -1,7 +1,15 @@
 const event = require('./routes/event');
 const map = require('./routes/map');
+const chart = require('./routes/chart');
+const search = require('./routes/search');
 
 module.exports = ((app) => {
-  app.use('/', event);
-  app.use('/map', map);
+  app.use('/index.xsjs', (req, res) => {
+    res.json({ message: 'Hello World' });
+  });
+
+  app.use('/event.xsjs', event);
+  app.use('/map.xsjs', map);
+  app.use('/search.xsjs', search);
+  app.use('/chart.xsjs', chart);
 });
